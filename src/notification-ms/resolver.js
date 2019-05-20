195182b63b905dd85cd74ac66cbde9ce3ca5fa69
Toño <ajsuarezf@unal.edu.co>
2019-05-20 14:01:40 -0500

    Understanding GrapghQL. Trying to write requests

---
+++ src/notification-ms/resolver.js
@@ -0,0 +1,20 @@
+import { generalRequest, getRequest, generalRequest2 } from '../utilities';
+import { url, port, entryPoint } from './server';
+
+const URL = `http://${url}:${port}`;
+const URL_LOGIN = `http://${url}:${port}/${entryPoint}`;
+
+const resolvers = {
+	Query: {
+		notifSend: (_) =>
+			getRequest(URL, ''),
+		login: (_, { id, token }) =>
+			generalRequest2(`${URL_LOGIN}/${id}/`, 'GET',_,token),
+	},
+	Mutation: {
+		createToken: (_, { user }) =>
+			generalRequest(URL_LOGIN, 'POST', user),
+	}
+};
+
+export default resolvers;
