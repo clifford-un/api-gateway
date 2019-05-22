d3f4225eea8c15038f98946beed39f3a6728bc01
dieguischa <dfchacona@unal.edu.co>
2019-05-21 19:45:41 -0500

    friends requests

--- src/users-ms/resolvers.js
+++ src/users-ms/resolvers.js
@@ -1,8 +1,10 @@
 import { generalRequest, getRequest } from '../utilities';
-import { url, port, entryPoint_user,entryPoint_test } from './server';
+import { url, port, entryPoint_user,entryPoint_test,entryPoint_friend } from './server';
 
 const URL_USER = `http://${url}:${port}/${entryPoint_user}`;
 const URL_TEST = `http://${url}:${port}/${entryPoint_test}`;
+const URL_FRIEND = `http://${url}:${port}/${entryPoint_friend}`;
+const URL_GET_FRIENDS = `http://${url}:${port}/see_friends/?`;
 
 const resolvers = {
 	Query: {
@@ -10,14 +12,20 @@ const resolvers = {
 			getRequest(URL_USER, ''),
 		allTests: (_) =>
 			getRequest(URL_TEST, ''),	
-		// userById: (_, { id }) =>
-		// 	generalRequest(`${URL_USER}/${id}/`, 'GET'),
+		allFriends: (_) =>
+			getRequest(URL_FRIEND, ''),
+		userById: (_, { id }) =>
+			 generalRequest(`${URL_USER}/${id}/`, 'GET'),
+		friendsById: (_, { id }) =>
+			 generalRequest(`${URL_GET_FRIENDS}id=${id}`, 'GET'),
 	},
 	Mutation: {
 		createTest: (_, { test }) =>
 			generalRequest(`${URL_TEST}/`, 'POST', test),
 		createUser: (_, { user }) =>
 			generalRequest(`${URL_USER}/`, 'POST', user),
+		createFriend: (_, { friend }) =>
+			generalRequest(`${URL_FRIEND}/`, 'POST', friend),
 		deleteUser: (_, { id }) =>
 			generalRequest(`${URL_USER}/${id}/`, 'DELETE'),
 		updateUser: (_, { id, user }) =>
