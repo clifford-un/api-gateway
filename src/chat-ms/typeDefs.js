d7bb99fbb517d890abadad413c8cb9381cf56003
Juanito Becerra <jsbecerrab@unal.edu.co>
2019-05-24 17:13:52 -0500

    chat-ms model changes

--- src/chat-ms/typeDefs.js
+++ src/chat-ms/typeDefs.js
@@ -2,7 +2,7 @@ export const chatsTypeDef = `
 type Chat {
     id: String!
     chat_user_origin: Int!
-    chat_room_id: Int!
+    chat_room_id: String!
     chat_text: String!
     chat_hidden: Boolean!
     chat_date_stamp: String!
@@ -13,13 +13,13 @@ type ChatTest{
 }
 input ChatInput {
     chat_user_origin: Int!
-    chat_room_id: Int!
+    chat_room_id: String!
     chat_text: String!
 }`;
 
 export const chatsQueries = `
     testChats: ChatTest!
-    chatById(chat_room_id: Int!): [Chat!]
+    chatById(chat_room_id: String!): [Chat!]
 `;
 
 export const chatsMutations = `
