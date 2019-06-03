1da4cb027a2a3b1c37ee43c2bca15334bf39a851
dieguischa <dfchacona@unal.edu.co>
2019-06-03 00:31:43 -0500

    :star2: Friend requests

--- src/users-ms/typeDefs.js
+++ src/users-ms/typeDefs.js
@@ -6,18 +6,31 @@ type Protected_User {
     user_name: String!
     e_mail: String!
     friends: [[Protected_User!]]
+    request_number: Int!
+    Friend_requests: [[[Protected_User]]]!
 }
 type User{
     user: Protected_User!
 }
 type Friend {
     user_id1: Int!
-    user_id2: Int!
-   
+    user_id2: Int!  
+}
+type Request {
+    user_id_to: Int!
+    user_id_from: Int!  
 }
 type Test {
     id: Int!
     test: String!  
+}
+type Requests{
+    to: [Protected_User]
+    from: [Protected_User]
+}
+input RequestInput {
+    user_id_to: Int!
+    user_id_from: Int! 
 }
 input TestInput {
     test: String!
@@ -48,6 +61,7 @@ export const usersQueries = `
     allFriends: [Friend]!
     userById(id: Int!): User!
     friendsById(id: Int!): [User]!
+    requestsById(id: Int!): [[[Protected_User]]]
 `;
 
 export const usersMutations = `
@@ -55,6 +69,8 @@ export const usersMutations = `
     createUser(user: UserInput!): User!
     createFriend(friend: FriendInput!): Friend!
     deleteUser(id: Int!): Int
+    createFriendRequest(request: RequestInput!): Request!
     updateUser(id: Int!, user: UserUpdateInput!): User!
-    
+    accept(id: Int!): Int
+    deny(id: Int!): Int
 `;
