bd58a4643b2e3329a4418f6494435af01fd4679b
Juanito Becerra <36463265+jsbecerrab@users.noreply.github.com>
2019-06-18 22:39:28 -0500

    :rage: another stupid change tee-hee

--- src/chat-ms/resolvers.js
+++ src/chat-ms/resolvers.js
@@ -1,4 +1,4 @@
-import { generalRequest, getRequest } from '../utilities';
+import { generalRequest, getRequest, getRequestProtected } from '../utilities';
 import { url, port, entryPoint } from './server';
 
 const URL = `http://${url}:${port}/${entryPoint}`;
