31439d1cbca6547f43566e9ce6f74f70a6f4c189
Juanito Becerra <36463265+jsbecerrab@users.noreply.github.com>
2019-06-20 08:46:48 -0500

    :rage: i'm stupid

--- src/chat-ms/resolvers.js
+++ src/chat-ms/resolvers.js
@@ -12,7 +12,7 @@ const resolvers = {
 	},
 	Mutation: {
 		createChat: (_, { chat, token, username }) =>
-			generalRequestProtected(`${URL}/`, 'POST', chat ),
+			generalRequestProtected(`${URL}/`, 'POST', token, username, chat ),
 		deleteChat: (_, { id,  token, username }) =>
 			generalRequestProtected(`${URL}/d/${id}/`, 'DELETE', token, username)
 	}
