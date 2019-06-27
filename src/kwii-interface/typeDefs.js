f96544424c8d7e086260fca3eb67d4d18c8de2d5
juanito becerra <jsbecerrab@unal.edu.co>
2019-06-27 01:03:58 -0500

    wii

--- src/kwii-interface/typeDefs.js
+++ src/kwii-interface/typeDefs.js
@@ -3,15 +3,9 @@ type Tweet {
     content: String!
     createdAt: String!
 
-}
-type name: String!
-
-type Tweets {
-  name: Tweet!
-}
-`;
+}`;
 
 export const interfaceQueries = `
     testInterface: String!
-    getTweet(email: String!): [Tweets!]
+    getTweet(email: String!): [Tweet!]
 `;
